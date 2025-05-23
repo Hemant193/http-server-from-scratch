760b780963b74f972a2367e8a8c53f8359653ffb
Hemant Mishra <hemishra555@gmail.com>
2025-05-23 14:04:04 +0200

    setup TCP server at port

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"log"
+)
 
 func main()  {
-	fmt.Println("Hello World!")
+	
+	// Create a listener
+	connection, error := net.Listen("tcp", ":8080")
+
+	if error != nil {
+		log.Fatalf("Failed to start server at port: %v", error)
+	}
+	defer connection.Close()
+	log.Println("Server started at port 8080")
+
 }
